38043f2d54c6448f6983897c3c748d984158f0d5
Manoel <manoel.lobo@gmail.com>
2019-05-22 10:30:25 -0300

    001 (#1)
    Merge challenge 001 to master

---
+++ src/components/001-signup/index.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import { FaGoogle, FaFacebookF } from 'react-icons/fa';
+
+import { Container, Field, GlobalStyle } from './style';
+
+const SignUp = () => (
+  <Container>
+    <GlobalStyle />
+    <div className="social-signup">
+      <h1>Join THE-APP</h1>
+
+      <span>Sign up with your social accounts</span>
+      <div className="social-buttons">
+        <a href="/">
+          <span>Join with yor Google account</span>
+          <FaGoogle aria-hidden />
+        </a>
+        <a href="/">
+          <span>Join with yor Facebook account</span>
+          <FaFacebookF aria-hidden />
+        </a>
+      </div>
+    </div>
+    <div className="email-signup">
+      <form>
+        Or use your email
+        <Field>
+          <label htmlFor="email">Email</label>
+          <input
+            id="email"
+            name="email"
+            type="email"
+            placeholder="you@yourmail.com"
+          />
+        </Field>
+        <Field>
+          <label htmlFor="password">Password</label>
+          <input
+            id="password"
+            name="password"
+            type="password"
+            placeholder="********"
+          />
+        </Field>
+        <button>Sign Up</button>
+      </form>
+    </div>
+  </Container>
+);
+
+export default SignUp;
