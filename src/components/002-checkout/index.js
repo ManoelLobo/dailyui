fa399a84bec8550068debfe3e2d9cb34694f1959
Manoel <manoel.lobo@gmail.com>
2019-05-23 18:35:11 -0300

    Personal data fields

--- src/components/002-checkout/index.js
+++ src/components/002-checkout/index.js
@@ -10,7 +10,42 @@ const Checkout = () => (
       <hr />
       <span className="user-email">john@smith.com</span>
     </Header>
-    <Main>Ok</Main>
+    <Main>
+      <form>
+        <label htmlFor="cardholder">Name</label>
+        <input
+          type="text"
+          name="cardholder"
+          id="cardholder"
+          placeholder="Name"
+          className="first-liner"
+        />
+        <label htmlFor="address">Address</label>
+        <input type="text" name="address" id="address" placeholder="Address" />
+
+        <div className="halfer">
+          <label htmlFor="city">City</label>
+          <input
+            type="text"
+            name="city"
+            id="city"
+            placeholder="City"
+            className="bottom-liner-left"
+          />
+
+          <label htmlFor="postalcode">Name</label>
+          <input
+            type="text"
+            name="postalcode"
+            id="postalcode"
+            placeholder="Postal Code"
+            className="bottom-liner-right"
+          />
+        </div>
+
+        <hr />
+      </form>
+    </Main>
   </Container>
 );
 
--- src/components/002-checkout/style.js
+++ src/components/002-checkout/style.js
@@ -8,6 +8,7 @@ export const Container = styled.div`
   background: ${gray[100]};
 
   font-size: 0.8rem;
+  color: ${gray[900]};
 
   display: flex;
   flex-direction: column;
@@ -29,6 +30,7 @@ export const Header = styled.header`
 
   hr {
     width: 50%;
+    border: 1px solid ${gray[300]};
   }
 
   .user-email {
@@ -38,6 +40,56 @@ export const Header = styled.header`
 `;
 
 export const Main = styled.main`
+  display: flex;
   flex: 2;
+  width: 100%;
   padding: 15px;
+
+  form {
+    display: flex;
+    flex: 1;
+    flex-direction: column;
+
+    label {
+      clip-path: inset(100%);
+      clip: rect(1px 1px 1px 1px); /* IE 6/7 */
+      clip: rect(1px, 1px, 1px, 1px);
+      height: 1px;
+      overflow: hidden;
+      position: absolute;
+      white-space: nowrap; /* added line */
+      width: 1px;
+    }
+
+    input {
+      width: 100%;
+      font-size: 0.7rem;
+      padding: 6px;
+      border: 1px solid ${gray[200]};
+      margin-bottom: -1px;
+
+      &.first-liner {
+        border-radius: 3px 3px 0 0;
+      }
+
+      &.bottom-liner-left {
+        border-radius: 0 0 0 3px;
+      }
+
+      &.bottom-liner-right {
+        border-radius: 0 0 3px 0;
+      }
+    }
+
+    .halfer {
+      display: flex;
+      width: 100%;
+      flex-direction: row;
+    }
+  }
+
+  hr {
+    width: 75%;
+    border: 1px solid ${gray[300]};
+  }
 `;
