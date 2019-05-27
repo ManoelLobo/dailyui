ac31a5eea1bcbb24128b95052b144adf23b23282
Manoel <manoel.lobo@gmail.com>
2019-05-27 08:37:12 -0300

    Basic styles to the main blocks

--- src/components/003-landing/style.js
+++ src/components/003-landing/style.js
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { primary, secondary, gray } from '../../style/colors';
 
 export const Container = styled.div`
   display: flex;
@@ -20,13 +21,25 @@ export const Content = styled.div`
 
 export const Main = styled.main`
   flex: 2;
+  padding: 20px;
+
+  background: ${gray[300]};
+  color: ${gray[800]};
 `;
 
 export const Header = styled.header`
   flex: 3;
+  padding: 20px;
+
+  background: ${secondary[400]};
+  color: ${secondary[100]};
 `;
 
 export const SignUp = styled.div`
   flex: 1;
   min-width: 300px;
+  padding: 20px;
+
+  background: ${primary[700]};
+  color: ${primary[200]};
 `;
