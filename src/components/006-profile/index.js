d9ce2b3b3abb24ef3d9e161a801f521ab6753aa6
Manoel <manoel.lobo@gmail.com>
2019-06-03 10:55:32 -0300

    A card-like User profile

--- src/components/006-profile/index.js
+++ src/components/006-profile/index.js
@@ -1,9 +1,33 @@
 import React from 'react';
 import { Container } from './style';
+import { IoMdPin, IoLogoGithub, IoLogoTwitter } from 'react-icons/io';
 
 const UserProfile = () => (
   <Container>
-    <h1>User Profile</h1>
+    <img
+      src="https://avatars0.githubusercontent.com/u/8916632?v=4"
+      alt="User avatar"
+    />
+    <div className="user-info">
+      <span className="name">Manoel Lobo</span>
+      <span className="title">Software Developer</span>
+      <span className="location">
+        <IoMdPin aria-hidden />
+        Campina Grande, Brazil
+      </span>
+      <div className="social">
+        <span className="github">
+          <a href="">
+            <IoLogoGithub aria-label="Github profile" />
+          </a>
+        </span>
+        <span className="twitter">
+          <a href="">
+            <IoLogoTwitter aria-label="Twitter feed" />
+          </a>
+        </span>
+      </div>
+    </div>
   </Container>
 );
 
--- src/components/006-profile/style.js
+++ src/components/006-profile/style.js
@@ -1,3 +1,55 @@
 import styled from 'styled-components';
+import { primary, secondary, gray } from '../../style/colors';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  display: flex;
+  width: 400px;
+  background: ${gray[100]};
+  border-radius: 6px;
+  padding: 20px;
+
+  img {
+    width: 96px;
+    height: 96px;
+    border-radius: 50%;
+    margin-right: 20px;
+  }
+
+  .user-info {
+    display: flex;
+    flex: 1;
+    flex-direction: column;
+    line-height: 1.5rem;
+
+    .name {
+      font-family: Raleway, sans-serif;
+      font-size: 1.5rem;
+      color: ${primary[800]};
+    }
+
+    .title {
+      font-size: 0.9rem;
+      color: ${primary[500]};
+    }
+
+    .location {
+      display: flex;
+      align-items: center;
+
+      font-size: 0.7rem;
+      color: ${gray[600]};
+    }
+
+    .social {
+      display: flex;
+      flex-direction: row;
+
+      font-size: 1rem;
+
+      a {
+        color: ${secondary[600]};
+        margin-right: 10px;
+      }
+    }
+  }
+`;
